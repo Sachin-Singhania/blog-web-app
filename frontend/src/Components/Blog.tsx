7465221de0b8f80026839bc6c931d78dc1237a91
Sachin Singhania <singhaniasachin160505@gmail.com>
2024-07-10 15:51:41 +0530

    design

--- frontend/src/Components/Blog.tsx
+++ frontend/src/Components/Blog.tsx
@@ -79,60 +79,61 @@ const Showblog = ({ blog }: { blog: Blogtypes }) => {
   }
 
   return (
-    <div>
-      <div className="flex justify-center">
-        <div className="grid grid-cols-1 lg:grid-cols-12 gap-4 px-10 w-full pt-20 max-w-screen-2xl">
-          <div className="col-span-1 lg:col-span-4 order-1 lg:order-2">
-            <span className="mx-8 font-thin">Author</span>
-            <div className="flex">
-              <div>
-                <div className="text-xl font-bold ml-7 text-blue-900">
-                  {blog.author.name ? blog.author.name.toUpperCase() : 'Anonymous'}
-                  <br />
-                </div>
-              </div>
-            </div>
-          </div>
-
-          {/* Title and content block */}
-          <div className="col-span-1 lg:col-span-8 order-2 lg:order-1">
-            <div className=" flex text-5xl font-extrabold">
-              <input
-                type="text"
-                value={title}
-                onChange={handleTitleChange}
-                className={`bg-transparent border-b border-black outline-none ${!isAuthor ? 'pointer-events-none' : ''}`}
-              />
-              {isModified && isAuthor ? (
-                <>
-                  <button onClick={cancel} className=" ml-3">
-                    <img src={cross} alt="" className="w-8 shadow-sm" />
-                  </button>
-                  <Button id={blog.id} title={title} content={content} category={blog.category} />
-                  <button onClick={toggleToolbar} className="text-xl ml-3 border border-card bg-card rounded-full p-3 shadow-md">
-                    {showToolbar ? 'Hide Toolbar' : 'Show Toolbar'}
-                  </button>
-                </>
-              ) : null}
-            </div>
-            <div className="pt-4">
-               <p className="font-semibold">CATEGORY :-</p>
-              <span className="w bg-transparent text-blue-900 text-2xl  border-l pl-1 border-black outline-none">
-               {category}
-              </span>
-            </div>
-            <div className="pt-4">
-              <JoditEditor
-                ref={editor}
-                value={content}
-                config={config}
-                onBlur={handleContentChange}
-              />
-            </div>
+   <div className="flex justify-center pt-20">
+  <div className="flex flex-col lg:flex-row gap-4 px-4 sm:px-6 lg:px-10 w-full max-w-screen-2xl">
+    <div className="lg:w-1/3 order-1 lg:order-2">
+      <span className="mx-4 sm:mx-6 lg:mx-8 font-thin">Author</span>
+      <div className="flex">
+        <div>
+          <div className="text-lg sm:text-xl font-bold ml-4 sm:ml-6 lg:ml-7 text-blue-900">
+            {blog.author.name ? blog.author.name.toUpperCase() : 'Anonymous'}
+            <br />
           </div>
         </div>
       </div>
     </div>
+
+    <div className="lg:w-2/3 order-2 lg:order-1">
+      <div className="flex text-3xl sm:text-4xl lg:text-5xl font-extrabold h-28">
+        <textarea 
+          value={title}
+          //@ts-ignore
+          onChange={handleTitleChange}
+          className={`no-scrollbar h-28 w-full bg-transparent border-b border-black outline-none resize-none ${!isAuthor ? 'pointer-events-none' : ''}`}
+          rows={1}
+        />
+      </div>
+      <div className="flex mt-3">
+        {isModified && isAuthor ? (
+          <>
+            <button onClick={cancel} className="ml-3">
+              <img src={cross} alt="" className="w-6 sm:w-8 shadow-sm" />
+            </button>
+            <Button id={blog.id} title={title} content={content} category={blog.category} />
+            <button onClick={toggleToolbar} className="text-sm sm:text-xl ml-3 border border-card bg-card rounded-full p-2 sm:p-3 shadow-md">
+              {showToolbar ? 'Hide Toolbar' : 'Show Toolbar'}
+            </button>
+          </>
+        ) : null}
+      </div>
+      <div className="pt-4">
+        <p className="font-semibold">CATEGORY :-</p>
+        <span className="w bg-transparent text-blue-900 text-lg sm:text-2xl border-l pl-1 border-black outline-none">
+          {category}
+        </span>
+      </div>
+      <div className="pt-4">
+        <JoditEditor
+          ref={editor}
+          value={content}
+          config={config}
+          onBlur={handleContentChange}
+        />
+      </div>
+    </div>
+  </div>
+</div>
+
   );
 };
 
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -13,5 +13,17 @@ export default {
       }
     },
   },
-  plugins: [],
+  plugins: [
+    function({ addUtilities }) {
+      addUtilities({
+        '.no-scrollbar': {
+          '-ms-overflow-style': 'none',  /* Internet Explorer 10+ */
+          'scrollbar-width': 'none',     /* Firefox */
+        },
+        '.no-scrollbar::-webkit-scrollbar': {
+          'display': 'none',             /* Safari and Chrome */
+        },
+      });
+    },
+  ],
 }
